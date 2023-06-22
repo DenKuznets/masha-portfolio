a154a5e66e511c29ef4704ca11a4970063de4cc6
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-06-22 11:13:32 +0300

    projectlist edit

--- src/ts/projectsList.ts
+++ src/ts/projectsList.ts
@@ -7,22 +7,22 @@ const ProjectsList = [
         images: ["proj1-1.png", "proj1-2.png"],
         id: 1,
     },
-    {
-        type: "ui/ux",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["proj1-1.png", "proj1-2.png"],
-        id: 2,
-    },
-    {
-        type: "ui/ux",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["proj1-1.png", "proj1-2.png"],
-        id: 3,
-    },
+    // {
+    //     type: "ui/ux",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["proj1-1.png", "proj1-2.png"],
+    //     id: 2,
+    // },
+    // {
+    //     type: "ui/ux",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["proj1-1.png", "proj1-2.png"],
+    //     id: 3,
+    // },
     {
         type: "app",
         name: "project-name",
@@ -31,22 +31,22 @@ const ProjectsList = [
         images: ["proj2-1.png", "proj2-2.png"],
         id: 4,
     },
-    {
-        type: "app",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["proj2-1.png", "proj2-2.png"],
-        id: 5,
-    },
-    {
-        type: "app",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["proj2-1.png", "proj2-2.png"],
-        id: 6,
-    },
+    // {
+    //     type: "app",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["proj2-1.png", "proj2-2.png"],
+    //     id: 5,
+    // },
+    // {
+    //     type: "app",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["proj2-1.png", "proj2-2.png"],
+    //     id: 6,
+    // },
     {
         type: "graphic",
         name: "project-name",
@@ -55,22 +55,22 @@ const ProjectsList = [
         images: ["proj3-1.png", "proj3-2.png"],
         id: 7,
     },
-    {
-        type: "graphic",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["proj3-1.png", "proj3-2.png"],
-        id: 8,
-    },
-    {
-        type: "graphic",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["proj3-1.png", "proj3-2.png"],
-        id: 9,
-    },
+    // {
+    //     type: "graphic",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["proj3-1.png", "proj3-2.png"],
+    //     id: 8,
+    // },
+    // {
+    //     type: "graphic",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["proj3-1.png", "proj3-2.png"],
+    //     id: 9,
+    // },
     {
         type: "web",
         name: "project-name",
@@ -79,22 +79,22 @@ const ProjectsList = [
         images: ["web-design-1.png", "web-design-2.jpg"],
         id: 10,
     },
-    {
-        type: "web",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["web-design-1.png", "web-design-2.jpg"],
-        id: 11,
-    },
-    {
-        type: "web",
-        name: "project-name",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
-        images: ["web-design-1.png", "web-design-2.jpg"],
-        id: 12,
-    },
+    // {
+    //     type: "web",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["web-design-1.png", "web-design-2.jpg"],
+    //     id: 11,
+    // },
+    // {
+    //     type: "web",
+    //     name: "project-name",
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. ",
+    //     images: ["web-design-1.png", "web-design-2.jpg"],
+    //     id: 12,
+    // },
 ];
 
 export default ProjectsList;
